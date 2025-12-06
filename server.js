e00a820f4ceec7aa3293d3f321b0c503a2f7bbc2
Chris Carter <39208854+carter7@users.noreply.github.com>
2025-12-06 00:05:17 -0600

    Change server port from 5000 to 5001

--- server.js
+++ server.js
@@ -36,6 +36,6 @@ app.post('/events', async (req, res) => {
 });
 
 // Listen on PORT variable or default to 5000
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(5001, () => console.log(`Server running on port ${5001}`));
